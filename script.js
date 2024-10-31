f188893221492fe659e6d93bd30e534f9d6437c5
Pranav Bommi <pbommire@uwaterloo.ca>
2024-10-31 16:06:20 -0400

    tabs

--- script.js
+++ script.js
@@ -84,6 +84,8 @@ function compileLapsToCSV() {
 
 function shareLaps() {
     const csvContent = compileLapsToCSV();
+    csvContent = csvContent.replace(/\t/g, '%09')
     const mailtoLink = `mailto:?subject=Stopwatch Laps&body=${encodeURIComponent(csvContent)}`;
     window.location.href = mailtoLink;
+    // console.log(encodeURIComponent(csvContent));
 }
